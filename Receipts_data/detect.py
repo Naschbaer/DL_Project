1f745c1987594c7170ae575c13de9e6a2e48772d
Husain <husainkapadia2811@gmail.com>
2018-06-15 22:10:34 +0200

    code modified

--- Receipts_data/detect.py
+++ Receipts_data/detect.py
@@ -3,15 +3,19 @@ import cv2
 
 im = cv2.imread('D:/hack/img2.jpg')
 
-height, width = im.shape[:2]
-img = cv2.resize(im, (1000, 1500), interpolation=cv2.INTER_CUBIC)
+width, height = im.shape[:2]
+aspect_ratio = width/height
+print(width, height, aspect_ratio)
+if aspect_ratio < 2:
+    img = cv2.resize(im, (int(width/4), int(2*height/3)), interpolation=cv2.INTER_CUBIC)
+else:
+    img = cv2.resize(im, (int(width/4), int(3*height/2)), interpolation=cv2.INTER_CUBIC)
+
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#gray = cv2.GaussianBlur(gray, (5, 5), 0)
-edges = cv2.Canny(gray, 100, 250)
+edges = cv2.Canny(gray, 50, 200, apertureSize=3, L2gradient=True)
 
 krnl = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 2))
-#t, ret = cv2.threshold(gray, 0, 200, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, krnl)
 
 im, contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -26,11 +30,9 @@ op = np.zeros((28, 28, len(contours)))
 
 for cnt in contours:
     x[idx], y[idx], w[idx], h[idx] = cv2.boundingRect(cnt)
-    #cv2.drawContours(edges, contours, idx, [255, 255, 255], 2)
 
-    if (x[idx] > 0 and y[idx] > 0 and x[idx] < width and y[idx] < height and w[idx] > 3 and h[idx] > 15 and w[idx] < 100 and h[idx] < 50):
-        #print(x[idx]-5, y[idx]-10, w[idx], h[idx], idx)
-        #cv2.rectangle(gray, (x[idx]-5, y[idx]-10), (x[idx]+w[idx]+5, y[idx]+h[idx]+10), (200, 100, 50), 1)
+    if x[idx] > 10 and y[idx] > 10 and x[idx] < width and y[idx] < height and w[idx] > 3 and h[idx] > 15 and w[idx] < 100 and h[idx] < 75:
+        #cv2.rectangle(gray, (x[idx]-5, y[idx]-10), (x[idx]+w[idx]+5, y[idx]+h[idx]+10), (0, 0, 0), 1)
         points[idx] = [x[idx]-5, y[idx]-10, idx]
         roi = gray[y[idx]-10:y[idx]+h[idx]+10, x[idx]-5:x[idx]+w[idx]+5]
         op[:, :, idx] = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_CUBIC)
@@ -57,8 +59,8 @@ for i in range(0, len(points_rearranged)):
 
 ordered_points = np.delete(ordered_points, np.where(ordered_points[:, 0:1] == 0), axis=0)
 
-for i in range(0,len(ordered_points)):
-    cv2.imwrite("test"+str(i)+".jpg", op[:, :, ordered_points[i, 2].astype(int)])
+for i in range(0, len(ordered_points)):
+    cv2.imwrite('Receipts_data/test_2'+str(i)+'.jpg', op[:, :, ordered_points[i, 2].astype(int)])
 
 cv2.imshow('img', gray)
 cv2.waitKey(0)
--- Receipts_data/refine.py
+++ Receipts_data/refine.py
@@ -1,18 +1,18 @@
 import cv2
 import os, fnmatch
 
-listOfFiles = os.listdir('D:/hack/')
-pattern = "test*"
+listOfFiles = os.listdir('D:/hack/Receipts_data')
+pattern = "test_2*"
 
 for entry in listOfFiles:
     if fnmatch.fnmatch(entry, pattern):
-        im = cv2.imread('D:/hack/' + entry)
-        #im = cv2.GaussianBlur(im, (3, 3), 0)
-        #h, w = im.shape[:2]
-        print(entry)
+        im = cv2.imread('D:/hack/Receipts_data/' + entry)
+        #h, w, c = im.shape[:3]
+        #print(h,w,c)
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         t, ret = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        cv2.imwrite(entry, ret)
+        ret = cv2.GaussianBlur(ret, (3, 3), 0)
+        cv2.imwrite('Receipts_data/Bahaar/'+entry, ret)
 
 cv2.imshow('img', ret)
 cv2.waitKey(0)
